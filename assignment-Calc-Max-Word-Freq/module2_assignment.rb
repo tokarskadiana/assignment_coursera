44bf2d47ab0dd8721d0c4f6c3dfc86cc01dbb2fb
tokarskadiana <tokarskadiana@gmail.com>
2016-05-25 12:36:56 +0200

    Update module2_assignment.rb
    This file includes my own solution.

--- assignment-Calc-Max-Word-Freq/module2_assignment.rb
+++ assignment-Calc-Max-Word-Freq/module2_assignment.rb
@@ -13,8 +13,8 @@ class LineAnalyzer
       @highest_wf_count = words_found.values.max
       @highest_wf_words = []
       words_found.select{|k,v| v === @highest_wf_count }.each do |k,v|
-      @highest_wf_words << k
-    end
+        @highest_wf_words << k
+      end
   end
 end
 class Solution
@@ -37,13 +37,13 @@ class Solution
     @highest_count_across_lines = @highest_count_across_lines.max
     @highest_count_words_across_lines = @analyzers.select do |line_analyzer|
     line_analyzer.highest_wf_count == @highest_count_across_lines
-      end
+    end
     @highest_count_words_across_lines
   end
   def print_highest_word_frequency_across_lines()
     puts "The following words have the highest word frequency per line:"
     @highest_count_words_across_lines.each do |line_analyzer|
     puts "#{line_analyzer.highest_wf_words} (appears in line #{line_analyzer.line_number})"
-  end
+    end
   end
 end
